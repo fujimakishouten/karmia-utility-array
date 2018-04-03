514be56045b73a32e77ed7592507873d9019cbd1
Kazuma Fujimaki <fujimaki-k@fujimakishouten.com>
2018-04-03 03:56:11 +0000

    Fix d.ts file

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,11 @@
-declare module KarmiaUtility {
-    export class KarmiaUtilityArray {
-        unique(list: Array<any>): Array<any>;
-        count(list: Array<any>, item: any): number;
-        range(start: number, stop: number, step: number): Array<number>;
-        flip(list: Array<any>): Array<any>;
-        intersection(list1: Array<any>, list2: Array<any>): Array<any>;
-        difference(list1: Array<any>, list2: Array<any>): Array<any>;
-    }
+declare class KarmiaUtilityArray {
+    unique(list: Array<any>): Array<any>;
+    count(list: Array<any>, item: any): number;
+    range(start: number, stop?: number, step?: number): Array<number>;
+    flip(list: Array<any>): Array<any>;
+    intersection(list1: Array<any>, list2: Array<any>): Array<any>;
+    difference(list1: Array<any>, list2: Array<any>): Array<any>;
 }
+
+declare function karmia_utility_array(options: object): KarmiaUtilityArray;
+export = karmia_utility_array;
